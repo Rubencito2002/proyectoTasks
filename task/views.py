d30ef92cbdceeeab01263063307cadaa77a66c9c
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-08 10:18:45 +0100

    añadio formulario y actualizado las vista y templates

--- task/views.py
+++ task/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks/task_list.html', {'tasks': Task})
+
+    if request.method == 'POST' :
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_list.html', {'tasks': Task, 'form': form})
