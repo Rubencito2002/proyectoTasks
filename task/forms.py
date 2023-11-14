1b39469288cb5a042b402a8b32ff10e97880bc09
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-14 10:01:58 +0100

    update view y form

--- task/forms.py
+++ task/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Task
 
+# Usando modelForm
+"""
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'completed']
+"""
+# Usando Form
+class TaskForm(forms.Form):
+    tittle = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    completed = forms.BooleanField(required=False)
--- task/views.py
+++ task/views.py
@@ -3,21 +3,22 @@ from django.views import View
 from .models import Task
 from .forms import TaskForm
 
-#def task_list(request):
-#    tasks = Task.objects.all()
-#
-#    if request.method == 'POST' :
-#        form = TaskForm(request.POST)
-#        if form.is_valid():
-#            form.save()
-#            return redirect('task_list')
-#    else:
-#        form = TaskForm()
-#    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
+"""
+def task_list(request):
+    tasks = Task.objects.all()
 
+    if request.method == 'POST' :
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
+"""
 class TaskViews(View):
     tasks = Task.objects.all()
-    task_list_template = 'tasks/task_list.html'
+    #task_list_template = 'tasks/task_list.html'
 
     def actualizarTask(self):
         self.tasks = Task.objects.all()
@@ -31,7 +32,11 @@ class TaskViews(View):
     def post(self,request):
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
+            #form.save()
+            title = form.cleaned_data['tittle']
+            description = form.cleaned_data['description']
+            completed = form.cleaned_data['completed']
+            Task.objects.create(title = title, description = description, completed = completed)
             return redirect('task_list')
         tasks = Task.objects.all()
         return render(request, 'tasks/task_list.html', {'tasks': self.actualizarTask(), 'form': form})
